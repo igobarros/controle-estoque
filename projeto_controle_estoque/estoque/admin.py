5b4a353c2503905b7ea22663ce34d67f882a5725
Igo Pereira Barros <igorestacioceut@gmail.com>
2019-09-27 16:01:47 -0300

    TabularInline. Close #7

--- projeto_controle_estoque/estoque/admin.py
+++ projeto_controle_estoque/estoque/admin.py
@@ -3,9 +3,16 @@ from .models import Estoque, EstoqueItens
 
 
 
+
+class EstoqueItensInline(admin.TabularInline):
+
+	model = EstoqueItens
+	extra = 0
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
 
+	inlines = (EstoqueItensInline,)
 	list_display = ('__str__', 'nf')
 	search_fields = ('nf',)
 	list_filter = ('funcionario',)
