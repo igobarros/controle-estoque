6da923aee11e1424c1f2869c67694a7b4b6aee4c
Igo Pereira Barros <igorestacioceut@gmail.com>
2019-09-29 17:57:55 -0300

    Estoque estoque_saida_form.html. Close #24

--- projeto_controle_estoque/estoque/urls.py
+++ projeto_controle_estoque/estoque/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 	path('add/', views.estoque_entrada_add, name='estoque_entrada_add'),
 	path('saida/', views.estoque_saida_list, name='estoque_saida_list'),
 	path('saida/<int:pk>', views.estoque_saida_detail, name='estoque_saida_detail'),
+	path('saida/add/', views.estoque_saida_add, name='estoque_saida_add'),
 ]
